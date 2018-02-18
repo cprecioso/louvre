1d0086eaec0eae3b0739a854288cb2c9bafe7021
Carlos Precioso <kalprestito@gmail.com>
2018-02-18 02:11:57 +0100

    Simplify Pipeline

--- packages/louvre/src/Pipeline.ts
+++ packages/louvre/src/Pipeline.ts
@@ -1,43 +1,59 @@
-import { SrcOptions, DestOptions } from "vinyl-fs"
-import { Files, Transform, destTransform, sourceTransform } from "./Transform"
 import * as most from "most"
+import File from "vinyl"
+import vfs from "vinyl-fs"
+import { toObservable, fromObservable } from "./stream-helpers"
 
-class Pipeline implements PromiseLike<void> {
+export type TransformFunction = (
+  files: most.Stream<File>
+) => most.Stream<File | Promise<File>>
 
-  protected readonly _observable: Files
-
-  constructor(observable: Files = most.never()) {
-    this._observable = observable
-  }
-
-  then = ((...args: any[]) => this._observable.drain().then(...args)) as (typeof Promise.prototype.then)
-  catch = ((...args: any[]) => this._observable.drain().catch(...args)) as (typeof Promise.prototype.catch)
-
-  transform(...fns: Transform[]): Pipeline {
-    const newObservable = [...fns].reduce((obs, fn) => fn(obs), this._observable)
-    return new Pipeline(newObservable)
-  }
-
-  merge(...pipelines: Pipeline[]): Pipeline {
-    const observables = [...pipelines].map(p => p._observable)
-    return this.transform(initial => most.mergeArray([initial, ...observables]))
-  }
-
-  src(globs: string | string[], options?: SrcOptions): Pipeline {
-    if (globs.length === 0) return this
-    return this.transform(sourceTransform(globs, options))
-  }
-
-  dest(folder: string, options?: DestOptions): Pipeline {
-    return this.transform(destTransform(folder, options))
-  }
-
-  exec(): Promise<void> {
-    return this._observable.drain()
-  }
+class Pipeline {
+  constructor(
+    protected readonly _observable: most.Stream<File> = most.never()
+  ) {}
 
   static empty() {
     return new this()
+  }
+
+  compose(this: Pipeline, ...fns: TransformFunction[]): Pipeline {
+    const newObservable = fns.reduce(
+      (obs, fn) =>
+        fn(obs)
+          .map(f => Promise.resolve(f))
+          .awaitPromises(),
+      this._observable
+    )
+    return new Pipeline(newObservable)
+  }
+
+  merge(this: Pipeline, ...pipelines: Pipeline[]): Pipeline {
+    const observables = [...pipelines].map(p => p._observable)
+    return this.compose(initial => most.mergeArray([initial, ...observables]))
+  }
+
+  exec(this: Pipeline): Promise<File[]> {
+    return this._observable.reduce<File[]>((arr, f) => [...arr, f], [])
+  }
+
+  src(
+    this: Pipeline,
+    globs: string | string[],
+    options?: vfs.SrcOptions
+  ): Pipeline {
+    if (globs.length === 0) return this
+    return this.compose(files => {
+      const newFiles = toObservable<File>(vfs.src(globs, options))
+      return most.merge(files, newFiles)
+    })
+  }
+
+  dest(this: Pipeline, folder: string, options?: vfs.DestOptions): Pipeline {
+    return this.compose(files => {
+      const [observable, stream] = fromObservable(files)
+      stream.pipe(vfs.dest(folder, options))
+      return observable
+    })
   }
 
 }
--- packages/louvre/src/Transform/index.ts
+++
@@ -1,9 +0,0 @@
-import * as File from "vinyl"
-import { Stream } from "most"
-
-export type Files = Stream<File>
-export interface Transform { (files: Files): Files }
-export default Transform
-
-export { default as sourceTransform } from "./source"
-export { default as destTransform } from "./dest"
