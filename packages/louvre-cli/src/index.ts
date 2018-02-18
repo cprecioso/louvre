facaa0dbad954e7669160da8cd31f9b9088af273
Carlos Precioso <kalprestito@gmail.com>
2018-02-18 02:10:43 +0100

    Use ES Module Interop

--- packages/louvre-cli/src/index.ts
+++ packages/louvre-cli/src/index.ts
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
-import * as Liftoff from "liftoff"
-import * as minimist from "minimist"
+import Liftoff from "liftoff"
+import minimist from "minimist"
 import { jsVariants } from "interpret"
-import * as V8Flags from "v8flags"
+import V8Flags from "v8flags"
 import { ChildProcess } from "child_process"
 import { Semver } from "sver-compat"
 import chalk from "chalk"
